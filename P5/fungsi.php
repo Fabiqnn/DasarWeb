a766fb7dd470e4b4401c58943a31777d4c63362d
Fabiqnn <arema20056@gmail.com>
2024-09-26 07:45:59 +0700

    Percobaan 5 praktikum 3

--- P5/fungsi.php
+++ P5/fungsi.php
@@ -1,5 +1,5 @@
 <?php
-function perkenalan($nama, $salam) {
+function perkenalan($nama, $salam = "Assalamualaikum") {
     echo  $salam . ", ";
     echo "Perkenalkan, nama saya" . $nama ."<br>";
     echo "Senang berkenalan dengan anda<br>";
@@ -12,5 +12,5 @@ echo "<br>";
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
