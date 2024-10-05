1ca7dfeb113a94b765cf5263b1ca6b7e6b12d6d4
Fabiqnn <arema20056@gmail.com>
2024-10-05 17:36:26 +0700

    Percobaan 6 Praktikum 4 Bagian 5

--- P6/fungsi.php
+++ P6/fungsi.php
@@ -1,29 +1,16 @@
 <?php
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo "$salam ";
-    echo "Perkenalkan, nama saya ". $nama ."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
-}
-
-perkenalan("Hamdana", "Hallo");
-
-echo "<br>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat Pagi";
-
-perkenalan($saya, $ucapanSalam);
-
-echo "<br>";
-
-perkenalan($saya);
-echo "<br>";
-
-
-function hitungUmur($thn_lhr, $thn_sekarang) {
-    $umur = $thn_sekarang - $thn_lhr;
+function hitungUmur($thn_Lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_Lahir;
     return $umur;
 }
 
-echo "Umur saya adalah " . hitungUmur(2005, 2024);
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo "$salam ";
+    echo  "Perkenalkan, nama saya " . $nama . "<br/>";
+
+    echo "Saya berusia " . hitungUmur(2005, 2024). " tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+perkenalan("Fabian");
 ?>
