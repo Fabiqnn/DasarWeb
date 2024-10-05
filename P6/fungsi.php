8e876db4406d0fb11c6648f64a50e8616beedd40
Fabiqnn <arema20056@gmail.com>
2024-10-05 17:30:24 +0700

    Percobaan 6 Praktikum 4 Bagian 4

--- P6/fungsi.php
+++ P6/fungsi.php
@@ -17,4 +17,13 @@ perkenalan($saya, $ucapanSalam);
 echo "<br>";
 
 perkenalan($saya);
+echo "<br>";
+
+
+function hitungUmur($thn_lhr, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lhr;
+    return $umur;
+}
+
+echo "Umur saya adalah " . hitungUmur(2005, 2024);
 ?>
