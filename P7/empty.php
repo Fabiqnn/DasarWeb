d89f14988077546634ecb528965297c1bf330474
Fabiqnn <arema20056@gmail.com>
2024-10-10 09:47:16 +0700

    Percobaan 7 Praktikum 2 Bagian 2

--- P7/empty.php
+++ P7/empty.php
@@ -7,4 +7,12 @@
         echo "Array terdefinisi dan tidak kosong.";
     }
     
+    echo "<br>";
+
+    if (empty($nonExsistentVar)) {
+        echo "Variabel tidak terdefinisi atau kosong.";
+    } else {
+        echo "Variabel terdefinisi dan tidak kosong.";
+    }
+    
 ?>
