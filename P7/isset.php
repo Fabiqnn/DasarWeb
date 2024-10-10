6df346759d46fffce7ea5ae146d208a923d0f917
Fabiqnn <arema20056@gmail.com>
2024-10-10 08:02:16 +0700

    Percobaan 7 Praktikum 1 Bagian 2

--- P7/isset.php
+++ P7/isset.php
@@ -1,6 +1,14 @@
 <?php
-    $umur;
-    if (isset($umur) && $umur >= 18) {
+    $data = array("Nama" => "Jane", "Usia" => 25);
+    if (isset($data['Nama'])) {
+        echo "Nama: " . $data['Nama'];
+    } else {
+        echo "Variable 'nama; tidak ditemukan dalam array.";
+    }
+    
+    echo "<br>";
+    
+    if (isset($data['Usia']) && $data['Usia'] >= 18) {
         echo "Anda sudah dewasa.";
     } else {
         echo "Anda belum dewasa atau variable 'umur' tidak ditemukan.";
