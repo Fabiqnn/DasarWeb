4e4f20a25bd0c5d90f27bbee5c89861cccd5d968
Fabiqnn <arema20056@gmail.com>
2024-10-13 09:57:54 +0700

    Percobaan 7 Praktikum 5 Bagian 2

--- P7/regex.php
+++ P7/regex.php
@@ -11,8 +11,17 @@
     }
     echo "<br>";
 
-   
+    $pattern1 = '/[0-9]/';
+    $text1 = 'There are 123 apples';
+    if (preg_match($pattern1, $text1, $matches)) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
+    echo "<br>";
 
+    
 
+    
     
 ?>
