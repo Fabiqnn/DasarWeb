a0c6b74b2641943aa1704f0b6429a7f52f078cd5
Fabiqnn <arema20056@gmail.com>
2024-10-13 11:59:05 +0700

    Percobaan 7 Praktikum 7 Bagian 2

--- P7/form_validasi.php
+++ P7/form_validasi.php
@@ -1,32 +1,52 @@
-<?php 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nama = $_POST['nama'];
-        $email = $_POST['email'];
-        $errors = array();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input dengan Validasi</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+    <h1>Form Input dengan Validasi</h1>
+    <form action="proses_validasi.php" id="myForm" method="POST">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama">
+        <span id="nama-error" style="color: red;"></span> <br>
 
-        //validasi nama
-        if (empty($nama)) {
-            $errors[] = "Nama harus diisi.";
-        }
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+        <span id="email-error" style="color: red;"></span> <br>
 
-        //validasi email
-        if (empty($email)) {
-            $errors[] = "Email harus diisi.";
-        } elseif (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Format email tidak valid.";
-        }
+        <input type="submit" value="Submit">
+    </form>
 
-        //Jika ada kesalahan validasi
-        if (empty($errors)) {
-            foreach ($errors as $error) {
-                echo $error . "<br>";
-            }
-        } else {
-            // Lanjutkan dengan pemrosesan data jika semua validasi berhasil
-            // Misalnya, menyimpan data ke database atau mengirim email
-            echo "Data berhasil Nama = $nama, Email = $email";   
-        }
-        
-        
-    }
-?>
+    <script>
+        $(document).ready(function () {
+            $("#myForm").submit(function(event) {
+                var nama = $("#nama").val();
+                var email = $("#email").val();
+                var valid = true;
+
+                if (nama === "") {
+                    $("#nama-error").text("Nama harus diisi.");
+                    valid = false;
+                } else {
+                    $("#nama-error").text("");
+                }
+
+                if (email === "") {
+                    $("#email-error").text("Email harus diisi.");
+                    valid = false;
+                } else {
+                    $("#email-error").text("");
+                }
+
+                if (!valid) {
+                    (event.preventDefault());
+                }
+            });
+        });
+    </script>
+    
+</body>
+</html>
