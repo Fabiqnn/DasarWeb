a7646f2adedb8508484508ac391bf3c8378008a0
Fabiqnn <arema20056@gmail.com>
2024-10-20 14:35:52 +0700

    Percobaan 8 Praktikum 1 Bagian 1

---
+++ P8/upload.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])) {
+    $targetDir = "uploads/";
+    $targetFile = $targerDir . basename($_FILES['myFile']['name']);
+
+    if (move_uploaded_file($_FILES['myFile']['tmp_name'], $targetFile)) {
+        echo "FIle berhasil diunggah.";
+    } else {
+        echo "Gagal mengunggah file.";
+    }
+    
+} 
+  
+?>
