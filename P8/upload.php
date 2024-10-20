71dbabae7c669502177dc2a778e9de83bc82c23d
Fabiqnn <arema20056@gmail.com>
2024-10-20 15:04:07 +0700

    Percobaan 8 Praktikum 1 Bagian 3

--- P8/upload.php
+++ P8/upload.php
@@ -2,12 +2,19 @@
     if (isset($_POST['submit'])) {
         $targetDir = "uploads/";
         $targetFile = $targetDir . basename($_FILES['myFile']['name']);
+        $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-        if (move_uploaded_file($_FILES['myFile']['tmp_name'], $targetFile)) {
-            echo "FIle berhasil diunggah.";
+        $allowesExtensions = array("jpg", "jpeg", "png", "gif");
+        $maxSize = 5*1024*1024;
+
+        if (in_array($fileType, $allowesExtensions) && $_FILES['myFile']['size'] <= $maxSize) {
+            if (move_uploaded_file($_FILES['myFile']['tmp_name'], $targetFile)) {
+                echo "File berhasil diunggah.";
+            } else {
+                echo "Gagal mengunggah file";
+            }
         } else {
-            echo "Gagal mengunggah file.";
+            echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
         }
-        
     } 
 ?>
